d75e4f2f9ad63da224a2c7563bbd3b4638b2e537
thomas.smith03 <thomas.smith03@mymail.champlain.edu>
2024-05-01 21:48:45 -0400

    REFLECTION

--- Graphics Final/Assets/Scripts/ReflectionCameraController.cs
+++ Graphics Final/Assets/Scripts/ReflectionCameraController.cs
@@ -1,19 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ReflectionCameraController : MonoBehaviour
 {
     [SerializeField] private Camera mainCamera;
+    [SerializeField] private Transform waterPlane;
 
     void Update()
     {
         float transformX = mainCamera.transform.position.x;
-        float transformY = -mainCamera.transform.position.y;
+        float transformY = waterPlane.position.y - (mainCamera.transform.position.y - waterPlane.position.y);
         float transformZ = mainCamera.transform.position.z;
 
         transform.position = new Vector3(transformX, transformY, transformZ);
 
+        float rotationX = -mainCamera.transform.rotation.eulerAngles.x;
+        float rotationY = mainCamera.transform.rotation.eulerAngles.y;
+        float rotationZ = mainCamera.transform.rotation.eulerAngles.z;
 
+        transform.rotation = Quaternion.Euler(rotationX, rotationY, rotationZ);
     }
 }
